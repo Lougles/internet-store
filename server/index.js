f9ebc8946ef6b94e1d07b300970719308cfba86b
vovacelidze <louglescom@gmail.com>
2022-09-14 22:09:07 +0300

    connect to bd

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 const express = require('express');
 const sequelize = require('./db')
+const models = require('./models/models');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+console.log(__dirname,'/.env')
+
+const PORT = process.env.PORT || 1234;
 
 const test = process.env.PORT
 console.log(test);
 
 const app = express()
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+  res.status(200).json({message: 'WORKING'})
+})
 
 const start = async () => {
   try {
-    // await sequelize.authenticate();
-    // await sequelize.sync();
-    app.listen(process.env.PORT, () => console.log(`server is starting on port ${PORT}`));
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`server is starting on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
