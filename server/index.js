b447a024965993666821e813d8350e441718cc5a
vovacelidze <louglescom@gmail.com>
2022-09-16 00:03:07 +0300

    added controllers, connect routes with controllers, bd is working, fix some bugs with bd connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const sequelize = require('./db')
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandingMiddleware')
 
 const PORT = process.env.PORT || 1234;
 
@@ -15,6 +16,7 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use('/api',router)
+app.use(errorHandler)
 
 const start = async () => {
   try {
