e146c6050588e4b5ada537268ff4b445b101a844
vovacelidze <louglescom@gmail.com>
2022-09-17 16:49:41 +0300

    auth page has been done, next registration

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Container, Form, Card, Button, Row} from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
+import {REGISTRATION_ROUTE, LOGIN_ROUTE} from '../utils/consts';
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE
   return (
-    <div>
-      AUTH
-    </div>
+    <Container 
+      className="d-flex justify-content-center align-items-center"
+      style={{height: window.innerHeight - 54}}>
+      <Card style ={{width: 600}} className="p-5">
+        <h2 className="m-auto">Authorization</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control className="mt-3" placeholder="Enter your email..."/>
+          <Form.Control className="mt-3" placeholder="Enter your password..."/>
+          <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+            <div>
+              Don't have account? <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+            </div>
+            <Button className="mt-4" variant="outline-success">Enter</Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
